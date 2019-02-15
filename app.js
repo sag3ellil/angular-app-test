474c1fda96b1b4cf10b3645c8212e65f0fbdbbc2
bouraoui <bouraoui.zich@gmail.com>
2019-02-15 01:44:39 +0000

    calculator num for string

--- app.js
+++ app.js
@@ -3,11 +3,30 @@
 
 angular.module('firstapp',[])
 .controller('firstcontroller',function($scope)
-{$scope.name="bouraoui";
+{$scope.nom="bouraoui";
 $scope.sayHello =function(){
   return "hello every body";
 };
 
-});
+})
 
+.controller('calculatorController',function($scope)
+{
+  $scope.name="";
+  $scope.totalvalue=0;
+  $scope.displaynumeric=function(){
+    var totalnamevalue= calculnumeriquefromstring($scope.name);
+    $scope.totalvalue=totalnamevalue;
+  };
+
+  function calculnumeriquefromstring( string )
+  {
+    var stringvalue=0;
+    for(var i=0;i<string.length;i++)
+    {
+      stringvalue=stringvalue+string.charCodeAt(i);
+    }
+    return stringvalue;
+  }
+});
 })();
